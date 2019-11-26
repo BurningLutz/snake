0611fee7cdabdb2c3509d329afffe65cacead5fc
Lutz <lutz.l.burning@gmail.com>
2019-11-26 14:35:07 +0800

    inside Main, simply do some logging for gaming area

--- src/Data/GamingArea.purs
+++ src/Data/GamingArea.purs
@@ -12,22 +12,31 @@ import Prelude
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Except.Checked (ExceptV)
 import Data.Functor.Variant (SProxy(..))
-import Data.Identity (Identity)
 import Data.Variant (Variant, inj)
 import Type.Row (type (+))
 
 type WidthOutOfRange r = (widthOutOfRange :: String | r)
 type HeightOutOfRange r = (heightOutOfRange :: String | r)
 
+-- | a variant instance of WidthOutOfRange
 widthOutOfRange :: forall r. Variant (WidthOutOfRange + r)
 widthOutOfRange = inj (SProxy :: SProxy "widthOutOfRange") "width should be between 2 and 100"
 
+-- | a variant instance of HeightOutOfRange
 heightOutOfRange :: forall r. Variant (HeightOutOfRange + r)
 heightOutOfRange = inj (SProxy :: SProxy "heightOutOfRange") "height should be between 2 and 100"
 
 data GamingArea = GamingArea Int Int
 
-gamingArea :: forall r. Int -> Int -> ExceptV (WidthOutOfRange + HeightOutOfRange + r) Identity GamingArea
+-- | safely make a gaming area by validating width and height in advance
+gamingArea
+  :: forall r m
+   . Monad m
+  => Int -> Int
+  -> ExceptV (WidthOutOfRange + HeightOutOfRange + r) m GamingArea
 gamingArea w h | w < 2 || w > 100 = throwError widthOutOfRange
                | h < 2 || h > 100 = throwError heightOutOfRange
                | otherwise        = pure (GamingArea w h)
+
+instance showGamingArea :: Show GamingArea where
+  show (GamingArea w h) = "GamingArea(" <> show w <> ", " <> show h <> ")"
--- src/Main.purs
+++ src/Main.purs
@@ -1,1 +1,32 @@
 module Main where
+
+import Prelude
+
+import Control.Monad.Except.Checked (ExceptV, handleError, safe)
+import Data.GamingArea (HeightOutOfRange, WidthOutOfRange, gamingArea)
+import Effect (Effect)
+import Effect.Class (class MonadEffect)
+import Effect.Class.Console (logShow)
+import Type.Row (type (+))
+
+logGamingArea
+  :: forall r m
+   . MonadEffect m
+  => Int -> Int
+  -> ExceptV (WidthOutOfRange + HeightOutOfRange + r) m Unit
+logGamingArea w h = do
+  area <- gamingArea w h
+  logShow area
+
+main :: Effect Unit 
+main = do
+  let
+    handleAll = handleError
+      { widthOutOfRange : \s -> logShow s
+      , heightOutOfRange : \s -> logShow s
+      }
+
+  safe do
+    logGamingArea 0 20   # handleAll
+    logGamingArea 10 200 # handleAll
+    logGamingArea 20 30  # handleAll
